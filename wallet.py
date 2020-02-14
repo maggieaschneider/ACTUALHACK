0825546629145486f050eb3b501a07530648743f
Maggie <margaret.anne.schneider@gmail.com>
2020-02-14 08:52:01 -0500

    wallet

--- wallet.py
+++ wallet.py
@@ -1,33 +1,51 @@
 from tkinter import *
-class wallet(Frame):
-    def __init__(self):
-    def create_widgets(self):
-    def math(self):
-            if choice == "p":
-                total += 0.01
-                print("New balance: $%0.2f" % (total))
-            elif choice == "n":
-                total += 0.05
-                print("New balance: $%0.2f" % (total))
-            elif choice == "d":
-                total += 0.10
-                print("New balance: $%0.2f" % (total))
-            elif choice == "q":
-                total += 0.25
-                print("New balance: $%0.2f" % (total))
-            # print output if there is an invalid option(reprint menu as well)
-            elif choice != "e":
-                print("Invalid option.")
-                print("\tMENU")
-                print("(p) - deposit a penny")
-                print("(n) - deposit a nickel")
-                print("(d) - deposit a dime")
-                print("(q) - deposit a quarter")
-                print("(e) - exit program and give balance")
-            # print out after every input to ask user if they would like to deposit more money
-            choice = input("Please press a letter to select an option: ")
 
-        # print if the user wants to exit and leave
-        if choice == "e":
-            print("Final balance: $%0.2f" % (total))
-            print("Goodbye.")
+
+class wallet(Frame):
+    def __init__(self, master):
+        super(wallet, self).__init__(master)
+        self.grid()
+        self.total = 0
+        # self.penny = 0.01
+        # self.nickel = 0.05
+        # self.dime = 0.10
+        # self.quarter = 0.25
+        self.create_widgets()
+
+    def create_widgets(self):
+        Label(self,text = "Insert money:"
+              ).grid(row=0, column = 0)
+        self.p = Button(self,text="Add Penny:", command = self.penny)
+        self.p.grid(row=1, column = 0)
+        self.n = Button(self, text= "Add Nickel:", command = self.nickel)
+        self.n.grid(row=2, column = 0)
+        self.d = Button(self, text="Add Dime:", command=self.dime)
+        self.d.grid(row=3, column=0)
+        self.q = Button(self, text="Add Quarter:", command=self.quarter)
+        self.q.grid(row=4, column=0)
+        self.tota = Label(self, text = self.total)
+        self.tota.grid(row = 6, column = 0)
+
+    def penny(self):
+        self.total += 0.01
+        self.tota["text"] = "%.2f" % self.total
+    def nickel(self):
+        self.total+=0.05
+        self.tota["text"] = "%.2f" % self.total
+    def dime(self):
+        self.total+= 0.1
+        self.tota["text"] = "%.2f" % self.total
+    def quarter(self):
+        self.total+= 0.25
+        self.tota["text"] = "%.2f" % self.total
+
+    # def end(self):
+    #     new_msg = self.math(self.output.get(0.0))
+    #     self.output.delete(0.0)
+
+
+
+root = Tk()
+root.title("Piggy Bank!")
+app = wallet(root)
+root.mainloop()
