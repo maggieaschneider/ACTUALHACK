82e56a111e254c96ab79105a653efb901e8ec498
Maggie <margaret.anne.schneider@gmail.com>
2020-02-13 22:40:06 -0500

    start

---
+++ app_manager.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+class App_Manager (object):
+    def __init__(self):
+        print("Hi i want to kill myself")
+
+        self.app_choices = appList ("")
+
+    def setup_app_selector(self):
+        # should open to the next screen
+
+    def main():
